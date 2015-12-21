e47bde9cbf20e1282541319ffdb600eed098c290
Adrian Peterson Co <adrianpco@gmail.com>
2015-12-21 22:38:09 +0800

    Adds buildpack for Elixir

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,13 @@ use Mix.Config
 config :slack_topics, SlackTopics.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :slack_topics, SlackTopics.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_evn("SECRET_KEY_BASE"),
+  pool_size: 20
 
 # Do not print debug messages in production
 config :logger, level: :info
